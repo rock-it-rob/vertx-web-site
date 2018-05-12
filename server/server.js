f598a52bea44931c7fb2593d3f9863aeab86e541
Rob Benton <rob.benton7@gmail.com>
2018-05-12 11:53:16 -0500

    ssl works

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
-var $ = require('jquery')
+var serverOptions = {
+  'ssl': true,
+  'pemKeyCertOptions': {
+    'keyPath': './key.pem',
+    'certPath': './cert.pem'
+  }
+}
 
-var server = vertx.createHttpServer().requestHandler(function(req) {
-  req.response().end('working')
-})
+var server = vertx.createHttpServer(serverOptions)
+  .requestHandler(function(req) {
+    req.response().end('working')
+  })
   .listen(8080)
